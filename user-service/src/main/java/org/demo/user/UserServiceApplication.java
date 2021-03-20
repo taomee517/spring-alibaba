aa881630d14604daa3116cf7dbf5c862dcecd4b7
Administrator <taomee517@gmail.com>
2021-03-20 20:01:33 +0800

    集成user service

--- user-service/src/main/java/org/demo/user/UserServiceApplication.java
+++ user-service/src/main/java/org/demo/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package org.demo;
+package org.demo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
---
+++ user-service/src/main/java/org/demo/user/controller/AuthController.java
@@ -0,0 +1,15 @@
+package org.demo.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+
+    @GetMapping("index")
+    public String index(){
+        return "this is user service";
+    }
+}
